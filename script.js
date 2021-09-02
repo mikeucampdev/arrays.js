19f6c6795ce8515a0e6e955ad250975204976ed5
Mr Mike <hemontemayor.c02@ucamp.utel.edu.mx>
2021-09-02 18:07:26 -0500

    Calificaciones array

---
+++ script.js
@@ -0,0 +1,13 @@
+var arreglo = [1, 'cadena d texto', [1,2,3, 'Cadena'],true,false];
+console.log (arreglo);
+
+function promedio (calificaciones){
+    console.log (calificaciones);
+    /*console.log(calificaciones.length);*/
+
+    console.log (calificaciones [3])
+}
+
+promedio ([70,80,100,50]);
+promedio ([70,80,100,50,60,10]);
+promedio ([70,80,100,50],60,50,100);
