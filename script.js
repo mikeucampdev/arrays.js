2bb3ee55fd482cbd57ddb10f893a5ea050feaabe
Mr Mike <hemontemayor.c02@ucamp.utel.edu.mx>
2021-09-02 19:19:56 -0500

    Iteracion Foreach

--- script.js
+++ script.js
@@ -11,3 +11,24 @@ function promedio (calificaciones){
 promedio ([70,80,100,50]);
 promedio ([70,80,100,50,60,10]);
 promedio ([70,80,100,50],60,50,100);
+
+
+/*ITERACIONES*/
+console.log('ForOF');
+for ( var elemento of arreglo) {
+    console.log(elemento)
+}
+
+console.log(arreglo)
+
+//FOR EACH
+
+console.log ('for each');
+
+arreglo.forEach(function(elemento,indice){
+    if (indice==2){
+        console.log('El indicie 2 contiene : '+ elemento);
+    } else {
+    console.log (elemento, indice);
+    }
+});
